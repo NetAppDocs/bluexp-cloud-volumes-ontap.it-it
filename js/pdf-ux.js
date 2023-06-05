a0ae95a68d005401eef2591042bde1d05d01b06e
IE-Build <iebuild-bot[bot]@users.noreply.github.com>
2023-06-05 18:46:19 +0000

    Built from 109dfa57 NetAppDocs/netappdocs-build@af534919a753647e66c14efe83117bfe0222a557

--- js/pdf-ux.js
+++ js/pdf-ux.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     // intialize config variables
     let zipLevel = "";
 
-    let baseUrlSection = "/it-it/cloud-manager-cloud-volumes-ontap/pdfs/";
+    let baseUrlSection = "/it-it/bluexp-cloud-volumes-ontap/pdfs/";
     const flavor = $("body").data("flavor");
     if (flavor) {
       baseUrlSection += flavor + "/";
@@ -13,7 +13,7 @@ $(document).ready(function () {
     let zipFilename = "";
     let zipFileSize = 0;
     zipLevel = zipLevel !== ""?zipLevel:1;
-    zipFilename = zipFilename !== ''?zipFilename:"cloud-manager-cloud-volumes-ontap.it-it.zip";
+    zipFilename = zipFilename !== ''?zipFilename:"bluexp-cloud-volumes-ontap.it-it.zip";
     
     // remove container class from pdf tree not having any sub child
     $("#toggleContainerPdf li.pdf-ux-container").not(".active").removeClass("pdf-ux-container");
